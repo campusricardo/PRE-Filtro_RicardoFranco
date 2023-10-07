bfc41dc8789c328f06d8d00d1a51594d6616c77c
campusricardo <campusricardo2023@gmail.com>
2023-10-06 23:45:43 -0500

    feat: :sparkles: using MongoDB Client

--- backend/controllers/wasteobject.controller.js
+++ backend/controllers/wasteobject.controller.js
@@ -1,12 +1,26 @@
 import wasteSchema from "../models/mongoose/wasteObject.js";
+import {MongoClient} from 'mongodb';
+// or as an es module:
+// import { MongoClient } from 'mongodb'
+
+// Connection URL
 
 export const getWasteObjects = async (req, res ) => {
     try {
-        const waste = await wasteSchema.find();
+        const url = process.env.MONGO_URI;
+        const client = new MongoClient(url);
+        const dbName = 'standard-recycling-company';
+        await client.connect();
+        const db = client.db(dbName);
+        const collection = db.collection('wasteobjects');
+        const find = await collection.find().toArray();
+
+
+
 
         return res.status(200).json({
             status: "success",
-            result: waste
+            result: find
         })
     } catch (error) {
         console.log(error);
@@ -18,6 +32,7 @@ export const getWasteObjects = async (req, res ) => {
 
 export const addWasteObject = async (req, res) => {
     try {
+        
         const {name, bin} = req.body;
         const wasteObject = new wasteSchema({name, bin});
         await wasteObject.save();
